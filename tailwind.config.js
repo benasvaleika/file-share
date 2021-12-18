8253232c485c2df08e25ccc3cfc7bcb9db8b5386
benasvaleika <bentini113@gmail.com>
2021-12-19 00:44:37 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      rhd: ["Red Hat Display", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#406882",
+        secondary: {
+          one: "#6998AB",
+          two: "#B1D0E0",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
