2e1d886cb579b2308be437d361b662a775dbf52c
benasvaleika <bentini113@gmail.com>
2021-12-21 10:17:53 +0200

    Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+interface FooterProps {}
+
+export const Footer: React.FC<FooterProps> = ({}) => {
+  return (
+    <div className="flex justify-center w-full mt-4 text-white font-rhd text-sm">
+      <div className="flex">
+        <div>About</div>
+        <div>&nbsp;|&nbsp;</div>
+        <div>How to use</div>
+        <div>&nbsp;|&nbsp;</div>
+        <div>Terms of use</div>
+        <div>&nbsp;|&nbsp;</div>
+        <div>Github</div>
+      </div>
+    </div>
+  );
+};
