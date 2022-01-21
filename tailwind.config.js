f2f5101984a80e774baf18f6912518c6957ddec6
benasvaleika <bentini113@gmail.com>
2022-01-21 15:35:08 +0200

    Config files minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      rhd: ["Red Hat Display", "sans-serif"],
+      rhd: ['Red Hat Display', 'sans-serif'],
     },
     extend: {
       colors: {
-        base: "#1a374d",
-        primary: "#406882",
+        base: '#1a374d',
+        primary: '#406882',
         secondary: {
-          one: "#6998AB",
-          two: "#B1D0E0",
+          one: '#6998AB',
+          two: '#B1D0E0',
         },
       },
     },
