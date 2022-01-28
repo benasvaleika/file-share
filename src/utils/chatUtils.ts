ca4e923f52595f577a7567e4d97ab073283e5d29
benasvaleika <bentini113@gmail.com>
2022-01-28 20:09:13 +0200

    Single digit minutes bug fix

--- src/utils/chatUtils.ts
+++ src/utils/chatUtils.ts
@@ -1,5 +1,7 @@
 export const getChatCurrTime = () => {
   const date = new Date();
-  const hourMinStr = `${date.getHours()}:${date.getMinutes()}`;
+  const hourMinStr = `${date.getHours()}:${
+    date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()
+  }`;
   return hourMinStr;
 };
