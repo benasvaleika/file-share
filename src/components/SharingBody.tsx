190e20246acb97c5e164726f31a14891859ea31a
benasvaleika <bentini113@gmail.com>
2022-02-08 23:23:27 +0200

    fix: minimal Sharing body changes

--- src/components/SharingBody.tsx
+++ src/components/SharingBody.tsx
@@ -5,10 +5,11 @@ import { File } from './File';
 interface SharingBodyProps {}
 
 export const SharingBody: React.FC<SharingBodyProps> = () => {
-  const files = true;
+  const fileToggle = false;
+
   return (
     <>
-      {files ? (
+      {fileToggle ? (
         <div>
           <File fileName="New Shared File.file" />
           <File fileName="Another Shared File.file" />
@@ -16,14 +17,16 @@ export const SharingBody: React.FC<SharingBodyProps> = () => {
           <File fileName="Another Shared File.file" />
         </div>
       ) : (
-        <div className=" w-[calc(100%-1rem)] ml-2 h-[calc(100%-7rem)] cursor-pointer">
+        <div className=" w-[calc(100%-1rem)] ml-2 h-[calc(100%-7rem)] ">
           <div className="flex h-full justify-center items-center flex-col">
             <FaPlus
               size={100}
               className="text-secondary-one hover:text-secondary-two ease-in-out duration-200"
             />
             <div className="text-secondary-two font-rhd font-bold text-lg -mb-1">Add new files</div>
-            <div className="text-secondary-two font-rhd font-bold">(Shared with all users)</div>
+            <div className="text-secondary-two font-rhd font-bold">
+              (Or press on a user icon directly to share files)
+            </div>
           </div>
         </div>
       )}
