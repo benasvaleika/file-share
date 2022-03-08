797bebe6f9e78e785ae6b5075398eb64fff7206d
benasvaleika <bentini113@gmail.com>
2022-03-07 22:17:27 -0600

    add: Default ICE servers

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,13 @@ import { config as dotenv } from 'dotenv';
 dotenv();
 
 export const WS_URL = process.env.WS_URL;
+
+export const ICE_SERVERS = [
+  {
+    urls: [
+      'stun:stun.l.google.com:19302',
+      'stun:stun1.l.google.com:19302',
+      'stun:stun2.l.google.com:19302',
+    ],
+  },
+];
