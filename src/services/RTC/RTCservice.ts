9f71395246c3212cacd9dffa206eeea712ce2286
benasvaleika <bentini113@gmail.com>
2022-03-17 00:49:42 +0200

    add: RTC local datachennel messaging working init

--- src/services/RTC/RTCservice.ts
+++ src/services/RTC/RTCservice.ts
@@ -1,10 +1,15 @@
 import { iceServers } from '../../utils/constants';
 
+let receiveChannel: any;
+let sendChannel: any;
+
 export const createConnection = async () => {
   const localConnection = new RTCPeerConnection({ iceServers });
   const remoteConnection = new RTCPeerConnection({ iceServers });
-  const sendChannel = localConnection.createDataChannel('sendDataChannel');
+  sendChannel = localConnection.createDataChannel('sendDataChannel');
   sendChannel.binaryType = 'arraybuffer';
+
+  remoteConnection.ondatachannel = receiveChannelCallback;
 
   localConnection.addEventListener('icecandidate', async (event) => {
     console.log('Local ICE candidate: ', event.candidate);
@@ -27,4 +32,37 @@ export const createConnection = async () => {
   const rtcAnswer = remoteConnection.createAnswer();
   remoteConnection.setLocalDescription(await rtcAnswer);
   localConnection.setRemoteDescription(await rtcAnswer);
+
+  setTimeout(function () {
+    if (sendChannel.readyState === 'open') {
+      sendChannel.send('Data sent from initiator');
+      console.log('Some data sent from create connection');
+    } else {
+      console.log('Tried to send a message');
+      console.log(sendChannel);
+    }
+  }, 3000);
 };
+
+function receiveChannelCallback(event: RTCDataChannelEvent) {
+  receiveChannel = event.channel;
+  receiveChannel.onmessage = handleReceiveMessage;
+  receiveChannel.onopen = handleReceiveChannelStatusChange;
+  receiveChannel.onclose = handleReceiveChannelStatusChange;
+}
+
+function handleReceiveMessage(event: MessageEvent<any>) {
+  console.log('Message received');
+  console.log(event);
+}
+
+function handleReceiveChannelStatusChange(event: any) {
+  if (receiveChannel) {
+    console.log("Receive channel's status has changed to " + receiveChannel.readyState);
+  }
+}
+
+function sendMessage() {
+  const message = 'this is a message';
+  sendChannel.send(message);
+}
